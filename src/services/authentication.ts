90a5ab300b23fe4271890d64de652a000547018b
Felipe <36526084+felip2018@users.noreply.github.com>
2021-05-24 18:34:46 -0500

    add deploy config

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -2,12 +2,6 @@ import { google } from 'googleapis';
 import { Request } from 'express';
 import { buildCredentialsObject } from '../utils/mapper-utils';
 
-const CLIENT_ID = '468945521540-oknl01dvp4f8o7lkikfqkhb146j9q9q4.apps.googleusercontent.com';
-const CLIENT_SECRET = 'JiOrlY2OWpp4mCNPJcHp6TV5';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-
-const REFRESH_TOKEN = '1//04qgvHg7GUZhUCgYIARAAGAQSNwF-L9IrZ24De_GgpQPPNqflUftE43Su4DTY_YN1JL6JS4OA6WBWUzBUp4ZTPN_mEPZ4cVNT8OE';
-
 class AuthenticationService {
     public getOAuth2Client(request: Request) {
         const credentials = buildCredentialsObject(request);
